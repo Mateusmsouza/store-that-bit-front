729cdcf0ddeab0eb9d4543cb43b0b083d41e515a
Mateus Souza <mateus_msouza@outlook.com>
2022-12-24 18:02:22 -0300

    fix: using my fancy https

--- js/requisitions.js
+++ js/requisitions.js
@@ -5,7 +5,7 @@ fileInput.addEventListener('input', () => {
     formData.append('file_upload', fileInput.files[0]);
     console.log(fileInput.files[0])
 
-    axios.post('http://152.67.34.166:8000/api/file/update', formData, {
+    axios.post('https://152.67.34.166:8000/api/file/update', formData, {
     headers: {
         'Content-Type': 'multipart/form-data',
         'Access-Control-Allow-Origin': '*'
