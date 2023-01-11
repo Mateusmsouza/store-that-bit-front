390cf2c90de0feb76427282183c9716d21910b79
Mateus Souza <mateus_msouza@outlook.com>
2023-01-11 19:01:59 -0300

    oh yeah

--- js/requisitions.js
+++ js/requisitions.js
@@ -3,8 +3,7 @@ const fileInput = document.querySelector('#file');
 const fileButton = document.querySelector('#file-store-button');
 
 fileButton.addEventListener('click', () => {
-    console.log(fileInput);
-    if (fileInput.files == null){
+    if (fileInput.files.length == 0){
         alert('select a file');
         return;
     }
@@ -12,14 +11,13 @@ fileButton.addEventListener('click', () => {
     console.log(fileInput.files);
     const formData = new FormData();
     formData.append('file_upload', fileInput.files[0]);
-    console.log(fileInput.files[0])
 
     axios.post('https://storethatbit.hopto.org/api/file/update', formData, {
     headers: {
         'Content-Type': 'multipart/form-data',
         'Access-Control-Allow-Origin': '*'
     }}).then( response => {
-        showModal(response.data.uui);
+        showModal(response.data.uuid);
     })
     .catch( err => {
 
