c138e986186e0e3ed7b0b1637be994ebd0031a8e
Mateus Souza <mateus_msouza@outlook.com>
2023-01-10 21:03:02 -0300

    add css

--- js/requisitions.js
+++ js/requisitions.js
@@ -1,6 +1,12 @@
 const fileInput = document.querySelector('#file-store-input');
 
-fileInput.addEventListener('input', () => {
+const fileButton = document.querySelector('#file-store-button');
+
+fileButton.addEventListener('click', () => {
+    if (fileInput.files.length == 0){
+        alert('select a file');
+        return;
+    }
     const formData = new FormData();
     formData.append('file_upload', fileInput.files[0]);
     console.log(fileInput.files[0])
