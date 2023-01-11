5a102dfb2e9ee11b8ba71030696a2d68db91cf28
Mateus Souza <mateussouza@luizalabs.com>
2023-01-11 17:45:04 -0300

    partial modal

--- js/showmodal.js
+++ js/showmodal.js
@@ -2,10 +2,15 @@ const fileModal = document.querySelector(".fullpage-modal-container");
 
 fileModal.style.display = "none";
 
-function showModal(filename){
+function showModal(fileuuid){
     const file_informative = document.querySelector("#file_informative");
     
-    file_informative.textContent = `this is a test ${filename}`;
+    file_informative.textContent = `https://storethatbit.netlify.app/${fileuuid}`;
     fileModal.style.display = "table";
 
 };
+
+const closeButtonModal = document.querySelector(".close-btn");
+closeButtonModal.addEventListener("click", () => {
+    fileModal.style.display = "none";
+});
